23f7e3ad1684fa304758f0c7f911d32695cef682
Kamil Kisiela <kamil.kisiela@gmail.com>
2017-03-01 17:07:03 +0100

    Use module.dynamicImport

--- client/imports/app/app.module.ts
+++ client/imports/app/app.module.ts
@@ -5,19 +5,14 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 
-// Fake dynamic import
-import { FooModule } from './foo/foo.module';
-import { BarModule } from './bar/bar.module';
-import dynamicImport from '../fake-dynamic-import';
-
 @NgModule({
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       { path: '', redirectTo: 'home', pathMatch: 'full' },
       { path: 'home', component: HomeComponent },
-      { path: 'foo', loadChildren: dynamicImport(FooModule) },
-      { path: 'bar', loadChildren: dynamicImport(BarModule) }
+      { path: 'foo', loadChildren: () => module.dynamicImport('./foo/foo.module').then(({FooModule}) => FooModule) },
+      { path: 'bar', loadChildren: () => module.dynamicImport('./bar/bar.module').then(({BarModule}) => BarModule) },
     ])
   ],
   declarations: [
--- client/imports/fake-dynamic-import.ts
+++
@@ -1,7 +0,0 @@
-export default function fakeDynamicImport(what): () => Promise<any> {
-  return () => new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(what);
-    }, 5000);
-  });
-}
